76afe66bb81be2f3a0443a2f1581cc9ee3cafe8a
Ulia <uliadumler@gmail.com>
2018-06-01 01:17:27 +0400

    Основное дз 2

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ for (var i = 0; i < 3; i++) {
 
 document.write(daySum);*/
 
-var monthSum = prompt('Ваш бюджет на месяц?'),
+/*var monthSum = prompt('Ваш бюджет на месяц?'),
 		shopName = prompt('Название вашего магазина?'),
 		shopGoods = new Array(3),
 		mainList = {
@@ -32,9 +32,86 @@ var monthSum = prompt('Ваш бюджет на месяц?'),
 		},
 		daySum = monthSum/30;
 
-for (var i = 0, j = shopGoods.length; i < j; i++) {
+for (var i = 0; i < shopGoods.length; i++) {
 	shopGoods [i] = (prompt('Какой тип товаров будем продавать?'));
 }
 
-document.write(daySum);
+console.log(shopGoods);
+
+document.write(daySum);*/
+
+let monthSum = prompt('Ваш бюджет на месяц?'),
+		shopName = prompt('Название вашего магазина?'),
+		mainList = {
+			sum: monthSum,
+			name: shopName,
+			shopGoods: [],
+			employers: {},
+			open: true
+		},
+		daySum = monthSum/30,
+		time = 4;
+
+for (let i = 0; i < 3; i++) {
+
+	let a = prompt('Какой тип товаров будем продавать?');
+	
+	if (typeof(a) === 'string'  && typeof(a) !== null && a != '' && a.length < 50) {
+		console.log('Все верно!');
+		mainList.shopGoods [i] = a;
+	} else {
+		alert('Введите снова');
+		i--;
+	}
+}
+
+
+/*let i = 0;
+
+while (i < 3) {
+
+	let a = prompt('Какой тип товаров будем продавать?');
+	
+	if (typeof(a) === 'string'  && typeof(a) !== null && a != '' && a.length < 50) {
+		console.log('Все верно!');
+		mainList.shopGoods [i] = a;
+	} else {
+		alert('Введите снова');
+		i--;
+	}
+
+	i++
+}*/
+
+/*let i = 0;
+
+do {
+	let a = prompt('Какой тип товаров будем продавать?');
+	
+	if (typeof(a) === 'string'  && typeof(a) !== null && a != '' && a.length < 50) {
+		console.log('Все верно!');
+		mainList.shopGoods [i] = a;
+	} else {
+		alert('Введите снова');
+		i--;
+	}
+
+	i++;
+}
+
+while (i < 3)*/
+
+if (time < 0) {
+	console.log('Такого просто не может быть');
+} else if (time > 8 && time < 20) {
+	console.log('Время работать!');
+}	else if (time < 24) {
+	console.log('Уже слишком поздно!');
+} else {
+	console.log('В сутках только 24 часа');
+}
+
+console.log(mainList);
+
+alert(mainList.sum / 30);
 
