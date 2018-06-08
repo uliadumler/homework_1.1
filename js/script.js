5561cc0bc128e1547e3c47fe392dc1e5f1a6b3eb
Ulia <uliadumler@gmail.com>
2018-06-08 12:06:29 +0400

    Основное дз 4

--- js/script.js
+++ js/script.js
@@ -1,48 +1,7 @@
-/*var monthSum = prompt('Ваш бюджет на месяц?'),
-		shopName = prompt('Название вашего магазина?'),
-		shopGoods = [],
-		mainList = {
-			sum: monthSum,
-			name: shopName,
-			goods: shopGoods,
-			employers: {
-
-			},
-			open: true
-		},
-		daySum = monthSum/30;
-
-for (var i = 0; i < 3; i++) {
-	shopGoods.push(prompt('Какой тип товаров будем продавать?'));
-}
-
-document.write(daySum);*/
-
-/*var monthSum = prompt('Ваш бюджет на месяц?'),
-		shopName = prompt('Название вашего магазина?'),
-		shopGoods = new Array(3),
-		mainList = {
-			sum: monthSum,
-			name: shopName,
-			goods: shopGoods,
-			employers: {
-
-			},
-			open: true
-		},
-		daySum = monthSum/30;
-
-for (var i = 0; i < shopGoods.length; i++) {
-	shopGoods [i] = (prompt('Какой тип товаров будем продавать?'));
-}
-
-console.log(shopGoods);
-
-document.write(daySum);*/
-
 let monthSum,
 		shopName,
-		time;
+		time,
+		price;
 		
 function start() {
 	monthSum = prompt('Ваш бюджет на месяц?');
@@ -52,7 +11,7 @@ function start() {
 	}
 
 	shopName = prompt('Название вашего магазина?').toUpperCase();
-	time = 19;
+	time = 29;
 }		
 
 start();
@@ -62,111 +21,81 @@ let mainList = {
 	name: shopName,
 	shopGoods: [],
 	employers: {},
-	open: true,
-	discount: true
-},
-daySum = monthSum/30;
+	open: false,
+	discount: true,
+	shopItems: [],
+	chooseGoods: function chooseGoods () {
+		for (let i = 0; i < 3; i++) {
 
-function chooseGoods () {
-	for (let i = 0; i < 3; i++) {
-
-		let a = prompt('Какой тип товаров будем продавать?');
-		
-		if (typeof(a) === 'string'  && typeof(a) !== null && a != '' && a.length < 50) {
-			console.log('Все верно!');
-			mainList.shopGoods [i] = a;
-		} else {
-			alert('Введите снова');
-			i--;
+			let a = prompt('Какой тип товаров будем продавать?', '');
+			
+			if (typeof(a) === 'string'  && typeof(a) != null && a != '' && a.length < 50) {
+				console.log('Все верно!');
+				mainList.shopGoods [i] = a;
+			} else {
+				alert('Введите снова');
+				i--;
+			}
 		}
-	}
-}
-
-chooseGoods();
-
-
-/*let i = 0;
-
-while (i < 3) {
-
-	let a = prompt('Какой тип товаров будем продавать?');
-	
-	if (typeof(a) === 'string'  && typeof(a) !== null && a != '' && a.length < 50) {
-		console.log('Все верно!');
-		mainList.shopGoods [i] = a;
-	} else {
-		alert('Введите снова');
-		i--;
-	}
-
-	i++
-}*/
-
-/*let i = 0;
-
-do {
-	let a = prompt('Какой тип товаров будем продавать?');
-	
-	if (typeof(a) === 'string'  && typeof(a) !== null && a != '' && a.length < 50) {
-		console.log('Все верно!');
-		mainList.shopGoods [i] = a;
-	} else {
-		alert('Введите снова');
-		i--;
-	}
-
-	i++;
-}
-
-while (i < 3)*/
-
-function workTime(time) {
-	if (time < 0) {
-		console.log('Такого просто не может быть');
-	} else if (time > 8 && time < 20) {
-		console.log('Время работать!');
-	}	else if (time < 24) {
-		console.log('Уже слишком поздно!');
-	} else {
-		console.log('В сутках только 24 часа');
-	}
-}
-
-workTime(100);
-
-
-function calcDaySum(budget) {
-	return budget / 30;
-}
-
-alert(calcDaySum(mainList.sum));
-
-let price = 200;
-
-function getDiscount(sum) {
-	return sum * 0.8;
-}
-
-if (mainList.discount == true) {
-	alert ('Цена со скидкой: ' + getDiscount(price));
-}	else {
-	alert('Скидка не предоставлена. Но вы можете приобрести скидочную карту!');
-}
-
-function takeEmployers () {
-	for (let i = 1; i < 5; i++) {
-
-		let a = prompt('Введите имя сотрудника');
-		
-		if (typeof(a) === 'string'  && typeof(a) !== null && a != '' && a.length < 50) {
-			mainList.employers[i] = a;
+	},
+	workTime: function workTime(time) {
+		if (time < 0) {
+			console.log('Такого просто не может быть');
+		} else if (time > 8 && time < 20) {
+			console.log('Время работать!');
+			mainList.open = true;
+		}	else if (time < 24) {
+			console.log('Уже слишком поздно!');
 		} else {
-			alert('Введите снова');
-			i--;
+			console.log('В сутках только 24 часа');
 		}
+	},
+	dayBudget: function dayBudget() {
+		alert('Ежедневный бюджет ' + mainList.monthSum / 30);
+	},
+	makeDiscount: function makeDiscount() {
+		if (mainList.discount == true) {
+				price = (price / 100) * 80;
+		}
+	},
+	hierEmployers: function hierEmployers () {
+		for (let i = 1; i < 5; i++) {
+
+			let name = prompt('Введите имя сотрудника', '');
+			
+			if (typeof(name) === 'string'  && typeof(name) !== null && name != '' && name.length < 50) {
+				mainList.employers[i] = name;
+			} else {
+				alert('Введите снова');
+				i--;
+			}
+		}
+	},
+	chooseShopItems: function chooseShopItems() {
+
+	let items = prompt('Перечислите через запятую товары', '' );
+		
+		while (!isNaN(items) || items == null || items == '') {
+			items = prompt('Неверно! Перечислите через запятую товары снова', '' );
+		}
+
+		mainList.shopItems = items.split(',');
+		mainList.shopItems.push(prompt('Подождите еще', ''));
+		mainList.shopItems.sort();
+
 	}
+};
+
+mainList.chooseShopItems();
+
+document.write('У нас вы можете купить: ' + '</br>');
+
+mainList.shopItems.forEach(function(item, i, arr) {
+	document.write((i + 1) + ': ' + item + '</br>');
+});
+
+for (let key in mainList) {
+	console.log('Свойство ' + key + ' имеет значение ' + mainList[key]);
 }
 
-takeEmployers();
-
-console.log(mainList)
+console.log(mainList);
